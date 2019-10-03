2bd89da9591488022e777e3229e6059eebe1f1a0
Yves Robert Charles Thior <yvesthior@gmail.com>
2019-10-03 22:15:13 +0000

    first commit

---
+++ client/precache-manifest.4bb6cd5e2f564083c7c5cdb687fcda5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56da782611a4a22461ae7a6f7b78c65b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c93fd10515481df3968",
+    "url": "/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "856d8b60fcaedcdbbbdc",
+    "url": "/static/js/2.c9a7d5e0.chunk.js"
+  },
+  {
+    "revision": "7c93fd10515481df3968",
+    "url": "/static/js/main.bbf43362.chunk.js"
+  },
+  {
+    "revision": "4b0b06c31ec8559acd28",
+    "url": "/static/js/runtime-main.89647cbb.js"
+  }
+]);
